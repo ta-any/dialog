c9bcb631a7bb62813ec2c1c6553cc668800f2fa9
tany <49059282+ta-any@users.noreply.github.com>
2024-10-30 16:39:23 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-console.log('Done/text...')
-txt = 'Etiam facilisis in dui ac interdum. Praesent ullamcorper augue at diam placerat, tempor pharetra metus porta. Praesent pellentesque purus sit amet velit blandit eleifend. Praesent ultrices justo vitae nibh aliquam, quis tristique dolor ornare. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque mollis a velit et luctus. Suspendisse quis orci ut ipsum varius scelerisque. Duis volutpat nibh ac lacinia faucibus. Vivamus nec nisi sodales, vestibulum massa ac, euismod est. '
+let txt = 'Etiam facilisis in dui ac interdum. Praesent ullamcorper augue at diam placerat, tempor pharetra metus porta. Praesent pellentesque purus sit amet velit blandit eleifend. Praesent ultrices justo vitae nibh aliquam, quis tristique dolor ornare. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque mollis a velit et luctus. Suspendisse quis orci ut ipsum varius scelerisque. Duis volutpat nibh ac lacinia faucibus. Vivamus nec nisi sodales, vestibulum massa ac, euismod est. '
 
 class Tmp_msg{
 	constructor(txt, who = 'bot'){
@@ -7,7 +6,6 @@ class Tmp_msg{
 		this.who = who
 	}
 	get_value_txt(){
-		console.log(this.txt)
 		return this.txt
 	}
 	get_list_elements(){
@@ -19,6 +17,8 @@ class Tmp_msg{
 		}
 	}
 	render(){
+		if(this.txt.length <= 0 || /^\s+$/.test(this.txt)) return
+
 		let block = document.createElement('div')
 		let list = this.get_list_elements()
 		for (let i = 0; i < 2; i++) {
@@ -26,16 +26,13 @@ class Tmp_msg{
 			DIV.classList.add(list[i])
 			if(DIV.classList.contains('text')){
 				DIV.textContent = this.txt
-			}
+			} 
 			block.appendChild(DIV)
 		}
 		
 		block.classList.add('post')
 		dialog.appendChild(block)
 		
-	}
-	who(){
-
 	}
 }
 tmp = new Tmp_msg(txt)
@@ -46,7 +43,6 @@ function send_msg() {
 	
 	user = new Tmp_msg(txt, 'user')
 	user.render()
-	user.get_value_txt()
 
 	input.value = ''
 	input.focus()
@@ -59,3 +55,4 @@ send.addEventListener('click', (event) => {
 document.addEventListener( 'keyup', event => {
   if( event.code === 'Enter' ) send_msg();
 });
+
